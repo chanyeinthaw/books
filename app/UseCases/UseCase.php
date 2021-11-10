d84604c67f54e664a44ceece43ec4ae351dbc54b
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-11-10 20:20:42 +0630

    feat: added use case abstract class to improve code readability by avoiding `try..catch` block

---
+++ app/UseCases/UseCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\UseCases;
+
+use Exception;
+
+abstract class UseCase {
+    protected bool $silenceOnError = false;
+
+    public function run(mixed $input) : mixed {
+        try {
+            return $this->handler($input);
+        } catch (Exception $e) {
+            $e = $this->onError($e);
+            if ($e && !$this->silenceOnError) throw $e;
+        }
+
+        return null;
+    }
+
+    protected function onError(Exception $e) : Exception|false {
+        return $e;
+    }
+
+    protected abstract function handler(mixed $input) : mixed;
+}
