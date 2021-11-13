0815f923bb7dfe1ed6e60c018831328301f89b17
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-11-13 12:15:14 +0630

    test: added unit test cases for cover image use cases
    **tests**
    - store cover image
    - store cover image as name
    - delete cover image
    - delete non-existent cover image

---
+++ tests/Unit/DeleteCoverImageUseCaseTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\UseCases\CoverImage\DeleteCoverImageUseCase;
+use App\UseCases\CoverImage\StoreCoverImageUseCase;
+use Illuminate\Http\UploadedFile;
+use Tests\TestCase;
+
+class DeleteCoverImageUseCaseTest extends TestCase
+{
+    private string $fileURL;
+    private DeleteCoverImageUseCase $deleteCoverImage;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $fakeCoverImage = UploadedFile::fake()->create('image.jpg', 5120, 'image/jpg');
+        $storeCoverImage = new StoreCoverImageUseCase();
+        $this->fileURL = $storeCoverImage->run($fakeCoverImage);
+        $this->deleteCoverImage = new DeleteCoverImageUseCase();
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_delete_cover_image_should_success()
+    {
+        $status = $this->deleteCoverImage->run($this->fileURL);
+
+        $this->assertTrue($status);
+    }
+
+    public function test_delete_cover_image_should_fail()
+    {
+        $status = $this->deleteCoverImage->run('non existent file');
+
+        $this->assertFalse($status);
+    }
+}
---
+++ tests/Unit/StoreCoverImageUseCaseTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\UseCases\CoverImage\StoreCoverImageUseCase;
+use Illuminate\Http\UploadedFile;
+use Tests\TestCase;
+
+class StoreCoverImageUseCaseTest extends TestCase
+{
+    private UploadedFile $fakeCoverImage;
+    private StoreCoverImageUseCase $storeCoverImage;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->fakeCoverImage = UploadedFile::fake()->create('image.jpg', 5120, 'image/jpg');
+        $this->storeCoverImage = new StoreCoverImageUseCase();
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_store_cover_image_should_success()
+    {
+        $url = $this->storeCoverImage->run($this->fakeCoverImage);
+
+        $this->assertTrue(gettype($url) === 'string');
+    }
+
+    public function test_store_cover_image_as_name_should_success() {
+        $this->storeCoverImage->storeAs('pre-defined-name.jpg');
+
+        $url = $this->storeCoverImage->run($this->fakeCoverImage);
+
+        $this->assertTrue(gettype($url) === 'string');
+    }
+}
