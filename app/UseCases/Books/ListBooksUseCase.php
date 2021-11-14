eff800b0c8218b573f5dc4c24251fcfb297aee09
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-11-14 17:34:55 +0630

    fix: changed `ListBookUseCase`'s behaviour
    **old behaviour**
    - accepts `query`, `skip` and `limit`
    - returns array of `Book` model
    
    **changed behaviour**
    - accepts `query`
    - returns `LengthAwarePaginator`

--- app/UseCases/Books/ListBooksUseCase.php
+++ app/UseCases/Books/ListBooksUseCase.php
@@ -3,28 +3,22 @@
 namespace App\UseCases\Books;
 
 use App\Models\Book;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Lib\UseCase;
-use Illuminate\Database\Eloquent\Collection;
 
 class ListBooksUseCase extends UseCase {
-    protected function handler(mixed $input): array|Collection{
+    protected function handler(mixed $input): LengthAwarePaginator {
         /**
-         * @var ?string $query;
-         * @var ?int $skip
-         * @var ?int $limit
+         * @var ?string $input;
          */
-        extract($input);
-
-        if (!isset($skip)) $skip = 0;
-        if (!isset($limit)) $limit = 16;
 
         $bookQuery = Book::query();
-        if (isset($query)) {
-            $search = "%$query%";
+        if ($input) {
+            $search = "%$input%";
             $bookQuery->where('title', 'LIKE', $search)
                 ->orWhere('author', 'LIKE', $search);
         }
 
-        return $bookQuery->offset($skip)->limit($limit)->get();
+        return $bookQuery->paginate();
     }
 }
