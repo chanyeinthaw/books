aa95d378c88b1e11ad93f195586bed526d6fa6c8
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-11-17 13:15:47 +0630

    feat: added `xml` file download response macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Response::macro('xml', function ($content, $fileName = 'download.xml') {
+            $headers = [
+                'Content-type'        => 'text/xml',
+                'Content-Disposition' => 'attachment; filename=' . $fileName,
+            ];
+
+            return \Response::make($content, 200, $headers);
+        });
     }
 }
