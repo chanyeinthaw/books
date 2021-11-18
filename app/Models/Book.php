fe27a83f69728fd99eeb2f36ec1a03bf164f700c
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-11-18 19:25:26 +0630

    fix: changed pagination to 10 from 12 per books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
-    protected $perPage = 12;
+    protected $perPage = 10;
 
     protected $fillable = [
         'title',
