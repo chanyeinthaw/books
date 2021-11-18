5389b92928334fc4df990f8d4f6566f73cffc72b
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-11-18 19:28:37 +0630

    feat: configured material ui theme provider

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createInertiaApp } from '@inertiajs/inertia-react'
+import {createTheme, ThemeProvider} from "@mui/material";
+import {blueGrey, grey, orange, red} from "@mui/material/colors";
 
 createInertiaApp({
     resolve: name => import(`./Pages/${name}`).then(module => module.default),
     setup({el, App, props}) {
-        render(<App {...props} />, el)
+        const theme = createTheme({
+            palette: {
+                primary: blueGrey,
+                secondary: grey,
+                error: red,
+                warning: orange,
+            }
+        })
+
+
+        render(<ThemeProvider theme={theme}>
+            <App {...props} />
+        </ThemeProvider> , el)
     },
 }).then()
