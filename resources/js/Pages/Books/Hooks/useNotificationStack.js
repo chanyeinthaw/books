08d6135ec165aa01fcab5841c5d21c2418cc5880
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-11-21 19:03:55 +0630

    fix: removed `errors` from showing in notification stack

--- resources/js/Pages/Books/Hooks/useNotificationStack.js
+++ resources/js/Pages/Books/Hooks/useNotificationStack.js
@@ -3,13 +3,11 @@ import {useEffect} from "react";
 import {useSnackbar} from "notistack";
 
 export default function useNotificationStack() {
-    let { flash, errors } = usePage().props
+    let { flash } = usePage().props
     let { enqueueSnackbar } = useSnackbar()
 
     useEffect(() => {
-        if (flash?.message) enqueueSnackbar(flash.message.message, { variant: 'info', persist: false })
+        if (flash?.message) enqueueSnackbar(flash.message, { variant: 'info', persist: false })
         else if (flash?.error) enqueueSnackbar(flash.error.message, { variant: 'error', persist: false })
-
-        if (Array.isArray(errors)) errors.map(error => enqueueSnackbar(error, { variant: 'error', persist: false }))
     }, [])
 }
