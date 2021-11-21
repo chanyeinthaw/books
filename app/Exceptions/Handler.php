3ec68c383848510fa01134cef655af52128c46b1
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-11-22 00:41:12 +0630

    fix: remove custom exception render method

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,20 +36,4 @@ class Handler extends ExceptionHandler
      */
     public function register() {
     }
-
-    public function render($request, Throwable $e): Response {
-        $response = parent::render($request, $e);
-
-        if (!app()->environment(['local', 'testing']) && in_array($response->status(), [500, 503, 404, 403])) {
-            return Inertia::render('Error/Index', ['status' => $response->status()])
-                ->toResponse($request)
-                ->setStatusCode($response->status());
-        } else if ($response->status() === 419) {
-            return back()->with([
-                'message' => 'The page expired, please try again.',
-            ]);
-        }
-
-        return $response;
-    }
 }
